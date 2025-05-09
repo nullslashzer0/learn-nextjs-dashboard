de9db1f9383bfef64b288e9e34e3dbaee1c759d3
Robert Wadsworth <robert.wadsworth414@gmail.com>
2025-05-08 20:17:48 -0400

    feat - Chap 16 - Metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,12 @@ import Table from '@/app/ui/invoices/table';
 import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
+
 export default async function Page(props: {
     searchParams?: Promise<{
         query?: string;
